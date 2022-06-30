ef8ce574ea9839173390eafaec26c818a31ee9ab
Mikolaj <mikolaj.m.zielinski@student.put.poznan.pl>
2022-06-30 08:42:01 +0200

    Added path generation algorithm

--- src/utils.py
+++ src/utils.py
@@ -10,12 +10,12 @@ def get_abs_path(n_parent: int = 0):
 def steer_to(img, p1, p2):
 
     def calculate_line(p1, p2):
-        x0, y0 = p1
-        x1, y1 = p2
+        x0, y0 = p1.detach().cpu().numpy()
+        x1, y1 = p2.detach().cpu().numpy()
         a = (y1 - y0) / (x1 - x0)
         b = y0 - a * x0
 
-        x_range = abs(x0 - x1) - 1
+        x_range = round(abs(x0 - x1))
         x_array = np.linspace(x0 + 1, x1, num=x_range, endpoint=False, dtype=int)
 
         points = []
@@ -27,22 +27,22 @@ def steer_to(img, p1, p2):
 
     def check_oversection(img, points):
         for x, y in points:
-            if img[x, y, 0] == 255:
+            if img[x, y] > 0:
                 return False
         return True
 
     line = calculate_line(p1, p2)
 
-    return check_oversection(img, line)
+    return check_oversection(img.detach().cpu().numpy(), line)
 
 
-def lazy_states_contraction(path):
+def lazy_states_contraction(path, x_obs):
 
     can_connect = True
     while can_connect:
         for i in range(len(path) - 2):
 
-            if steer_to(path[0 + i], path[2 + i]):
+            if steer_to(x_obs, path[0 + i], path[2 + i]):
                 path.pop(i + 1)
                 can_connect = False
 
@@ -51,11 +51,11 @@ def lazy_states_contraction(path):
     return path
 
 
-def is_feasible(path):
+def is_feasible(path, x_obs):
 
     feasible = True
     for i in range(len(path) - 1):
-        if not steer_to(path[0 + i], path[1 + i]):
+        if not steer_to(x_obs, path[0 + i], path[1 + i]):
             feasible = False
 
     return feasible
